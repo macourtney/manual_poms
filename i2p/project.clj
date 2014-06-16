3a0ade8becff3479184ca16b42c7ab7bedb2d5c4
Matt Courtney <macourtney@gmail.com>
2014-06-15 19:14:55 -0700

    Added the bouncy castle dependency.

--- i2p/project.clj
+++ i2p/project.clj
@@ -1,3 +1,3 @@
 (defproject org.clojars.macourtney/i2p "0.9.13-0"
   :description "This is a dummy project to create a pom for i2p.jar"
-  :signing { :gpg-key "FB1913A1" })
+  :dependencies [[org.bouncycastle/bcprov-jdk16 "1.46"]])
